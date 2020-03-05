b9508c711bfcc9e631e7d1419d62f23419376e36
locnguyent3108 <locnguyent3108@gmail.com>
2020-03-05 10:51:14 +0700

    no message

---
+++ src/pages/FoodPage.java
@@ -0,0 +1,18 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class FoodPage extends BasePage{
+    public FoodPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public FoodPage(WebDriverWait wait, WebDriver driver) {
+        super(wait, driver);
+    }
+    //*********Constructor**********
+    //*********Page Variables*******
+    //*********Web Elements*********
+    //*********Page Methods*********
+}
---
+++ src/testcases/food/FoodAddEditTest.java
@@ -0,0 +1,16 @@
+package testcases.food;
+
+import org.testng.annotations.Test;
+import testcases.BaseTest;
+
+public class FoodAddEditTest extends BaseTest {
+    String UserSystem = "RecFullSys-Set-Y";
+    String UserCorp = "RecFullCorp-Set-Y";
+    String UserFacility = "RecFullFac-Set-Y";
+
+    @Test
+    public void foodAddEditTest() {
+
+    }
+
+}
